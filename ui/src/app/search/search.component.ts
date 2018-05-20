0f0469fcccce84f00581fbeb5ef1afb58f03150b
jasminejeane <jasminejeane@gmail.com>
2018-05-20 16:04:25 -0700

    starter ui added

---
+++ ui/src/app/search/search.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+// import 'rxjs/add/operator/toPromise';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  people: any; //add the public property here
+
+  constructor(
+      private http: Http
+  ) { }
+
+  findAstronauts(){
+    this.http.get('http://api.open-notify.org/astros.json')
+    .toPromise()
+    .then(response => this.people = response.json().people); //set it here
+    }
+
+  ngOnInit() {
+  }
+
+}
