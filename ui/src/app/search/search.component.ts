e31ac45dbdead48bd9386797560cbbc5d81bacfa
jasminejeane <jasminejeane@gmail.com>
2018-05-21 15:01:16 -0700

    app api connected with backend

--- ui/src/app/search/search.component.ts
+++ ui/src/app/search/search.component.ts
@@ -16,9 +16,13 @@ export class SearchComponent implements OnInit {
   ) { }
 
   findWeather(zip){
-     this.http.get('http://api.openweathermap.org/data/2.5/weather?zip=' + zip + ',us&appid=052f26926ae9784c2d677ca7bc5dec98&&units=imperial')
+     this.http.get('/api/users')
      .toPromise()
-     .then(response => this.weather = response.json()); // use it here
+     .then(response => console.log(
+       response.json()
+     )
+     )
+     // .then(response => this.weather = response.json()); // use it here
    }
 
 
